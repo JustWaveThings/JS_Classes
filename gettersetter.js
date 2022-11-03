523471bdb56d7d42c9e8d13a26eb39029ebc61cc
JustWaveThings <justwavethings@proton.me>
2022-11-03 13:34:13 -0400

    Add getter for age based on birthday

--- gettersetter.js
+++ gettersetter.js
@@ -27,7 +27,7 @@ user.fullName = 'Terry Silver';
 console.log(user.fullName);
 console.log(user.name); */
 
-let user = {
+/* let user = {
 	get name() {
 		return this._name;
 	},
@@ -45,3 +45,31 @@ user.name = 'Samantha';
 console.log(user.name);
 user.name = 'Sam';
 console.log(user.name);
+ */
+
+/* function User(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+let jerry = new User('Jerry', 42);
+
+console.log(jerry.age); */
+
+function AnotherUser(name, birthday) {
+	this.name = name;
+	this.birthday = birthday;
+
+	Object.defineProperty(this, 'age', {
+		get() {
+			let todayYear = new Date().getFullYear();
+			return todayYear - this.birthday.getFullYear();
+		},
+	});
+}
+
+let terry = new AnotherUser('Terry', new Date(1980, 0, 6));
+console.log(terry.name);
+console.log(terry.birthday);
+console.log({ terry });
+console.log(terry.age);
