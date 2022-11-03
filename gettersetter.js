93ea9814e8e27179bfa534268c1f82e8eb77bbef
JustWaveThings <justwavethings@proton.me>
2022-11-03 13:05:22 -0400

    Smarter getter setters

--- gettersetter.js
+++ gettersetter.js
@@ -9,7 +9,7 @@ let obj = {
 	},
 };
 
-let user = {
+/* let user = {
 	name: 'Joe',
 	surname: 'Zachs',
 
@@ -25,4 +25,23 @@ let user = {
 console.log(user.fullName);
 user.fullName = 'Terry Silver';
 console.log(user.fullName);
+console.log(user.name); */
+
+let user = {
+	get name() {
+		return this._name;
+	},
+
+	set name(value) {
+		if (value.length < 4) {
+			console.log('Name too short - must be at least 4 characters');
+			return;
+		}
+		this._name = value;
+	},
+};
+
+user.name = 'Samantha';
+console.log(user.name);
+user.name = 'Sam';
 console.log(user.name);
